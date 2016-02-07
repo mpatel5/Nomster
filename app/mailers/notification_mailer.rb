61cafd6c27038e7a0af47536217d0b557ac21192
Mitesh Patel <mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.name Mitesh>
2016-02-07 05:46:09 +0000

    Previewing automated email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "ken@thefirehoseproject.com",subject: "A comment has been added to your place")
+    mail(to: "mpatel5@icloud.com",subject: "A comment has been added to your place")
   end
 end
